bfb83dfcf1effa33d47ed5ee86978e15c869147a
nylecm <michalnylec@gmail.com>
2020-07-20 12:50:15 +0100

    Welcome Message Improved, player name input, and GRID_STATUS enum implemented.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,15 +1,42 @@
 import java.util.Scanner;
 
-public class Main {
+public class TicTacToe {
+    private static GRID_STATUS[][] gameBoard;
+    private static String player1Name;
+    private static String player2Name;
+
+    private enum GRID_STATUS {
+        X_CLAIMED,
+        O_CLAIMED,
+        UNCLAIMED
+    }
+
     public static void main(String[] args) {
         playTicTacToe();
     }
 
     private static void playTicTacToe() {
-        System.out.println("Welcome to TicTacToe.");
-        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to TicTacToe!\n");
 
         printInstructions();
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter player 1's (X) name: ");
+        player1Name = in.nextLine();
+        System.out.print("Please enter player 2's (O) name: ");
+        player2Name = in.nextLine();
+    }
+
+    private boolean isGameOver() {
+
+    }
+
+    private boolean isTie() {
+        // True When:
+    }
+
+    private boolean isWin() {
+        // True When
     }
 
     private static void printInstructions() {
